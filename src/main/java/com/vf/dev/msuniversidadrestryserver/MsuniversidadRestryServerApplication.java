8e759c4692a5819cc7e33a69abc741727f913cae
VFALAN <alanvillafan95@gmail.com>
2023-08-12 18:29:42 -0600

    primera carga

--- src/main/java/com/vf/dev/msuniversidadrestryserver/MsuniversidadRestryServerApplication.java
+++ src/main/java/com/vf/dev/msuniversidadrestryserver/MsuniversidadRestryServerApplication.java
@@ -1,15 +1,23 @@
 package com.vf.dev.msuniversidadrestryserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
+@Component
 public class MsuniversidadRestryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsuniversidadRestryServerApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MsuniversidadRestryServerApplication.class, args);
+    }
+
 
 }
